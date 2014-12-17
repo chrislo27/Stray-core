9951d9e376765e1fef790c884a4907d8b857d877
chrislo27 <kinchrislo@gmail.com>
2014-12-17 20:25:04 +0800

    Update chunk system

--- src/stray/world/Chunk.java
+++ src/stray/world/Chunk.java
@@ -1,5 +1,8 @@
 package stray.world;
 
+import stray.blocks.Block;
+import stray.blocks.Blocks;
+
 /**
  * Represents a 16x16 area
  * 
@@ -8,5 +11,23 @@ package stray.world;
  */
 public class Chunk {
 	
+	public static final int chunkSize = 16;
+	
+	Block[][] blocks = new Block[chunkSize][chunkSize];
+	String[][] meta = new String[chunkSize][chunkSize];
+	
+	int chunkX = 0, chunkY = 0;
+	
+	public Chunk(int x, int y){
+		chunkX = x;
+		chunkY = y;
+		
+		for(int bx = 0; bx < chunkSize; bx++){
+			for(int by = 0; by < chunkSize; by++){
+				blocks[bx][by] = Blocks.defaultBlock();
+				meta[bx][by] = null;
+			}
+		}
+	}
 	
 }
