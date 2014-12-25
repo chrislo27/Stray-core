6575657f6c5256ef0c26e431900ab4f005d32381
chrislo27 <kinchrislo@gmail.com>
2014-12-24 22:44:36 -0800

    Begin secret project

---
+++ src/stray/nukliex/NukliexScreen.java
@@ -0,0 +1,53 @@
+package stray.nukliex;
+
+import stray.Main;
+import stray.Updateable;
+
+
+public class NukliexScreen extends Updateable{
+
+	public NukliexScreen(Main m) {
+		super(m);
+	}
+
+	@Override
+	public void render(float delta) {
+	}
+
+	@Override
+	public void renderUpdate() {
+	}
+
+	@Override
+	public void tickUpdate() {
+	}
+
+	@Override
+	public void renderDebug(int starting) {
+	}
+
+	@Override
+	public void resize(int width, int height) {
+	}
+
+	@Override
+	public void show() {
+	}
+
+	@Override
+	public void hide() {
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
+	}
+
+	@Override
+	public void dispose() {
+	}
+
+}
