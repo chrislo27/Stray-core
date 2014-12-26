69f02d6686f39f371668fe685e1211d4e3a5998e
chrislo27 <kinchrislo@gmail.com>
2014-12-26 11:08:57 -0800

    Fix augment thing

--- src/stray/augment/Augment.java
+++ src/stray/augment/Augment.java
@@ -5,13 +5,18 @@ public abstract class Augment {
 	
 	
 	/**
-	 * called when the player presses the activate button (key just pressed)
+	 * called when the player just presses the activate button (key just pressed)
 	 */
-	public abstract void onJustActivate();
+	public abstract void onActivateStart();
 	
 	/**
 	 * called every frame when the activate button is DOWN
 	 */
-	public abstract void onHoldActivate();
+	public abstract void onActivate();
+	
+	/**
+	 * called when the player releases the activate button
+	 */
+	public abstract void onActivateEnd();
 	
 }
