48e0d2c2b2efd64937ecfbc48d5aa637ba0df5f3
chrislo27 <kinchrislo@gmail.com>
2014-12-26 11:00:53 -0800

    Add methods to Augment

--- src/stray/augment/Augment.java
+++ src/stray/augment/Augment.java
@@ -3,4 +3,15 @@ package stray.augment;
 
 public abstract class Augment {
 	
+	
+	/**
+	 * called when the player presses the activate button (key just pressed)
+	 */
+	public abstract void onJustActivate();
+	
+	/**
+	 * called every frame when the activate button is DOWN
+	 */
+	public abstract void onHoldActivate();
+	
 }
