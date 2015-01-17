266531144042eb9fb8a20ac8d78c027b1037b8f1
chrislo27 <kinchrislo@gmail.com>
2015-01-17 14:52:55 -0800

    Refactor to use gdx GL instead of GL20

--- src/stray/util/render/StencilMaskUtil.java
+++ src/stray/util/render/StencilMaskUtil.java
@@ -9,30 +9,30 @@ public class StencilMaskUtil {
 	 * call this BEFORE rendering with ShapeRenderer and BEFORE drawing sprites, and AFTER what you want in the background rendered
 	 */
 	public static void prepareMask() {
-		Gdx.gl20.glDepthFunc(GL20.GL_LESS);
+		Gdx.gl.glDepthFunc(GL20.GL_LESS);
 
-		Gdx.gl20.glEnable(GL20.GL_DEPTH_TEST);
+		Gdx.gl.glEnable(GL20.GL_DEPTH_TEST);
 
-		Gdx.gl20.glDepthMask(true);
-		Gdx.gl20.glColorMask(false, false, false, false);
+		Gdx.gl.glDepthMask(true);
+		Gdx.gl.glColorMask(false, false, false, false);
 	}
 
 	/**
 	 * call this AFTER batch.begin() and BEFORE drawing sprites
 	 */
 	public static void useMask() {
-		Gdx.gl20.glDepthMask(false);
-		Gdx.gl20.glColorMask(true, true, true, true);
+		Gdx.gl.glDepthMask(false);
+		Gdx.gl.glColorMask(true, true, true, true);
 
-		Gdx.gl20.glEnable(GL20.GL_DEPTH_TEST);
+		Gdx.gl.glEnable(GL20.GL_DEPTH_TEST);
 
-		Gdx.gl20.glDepthFunc(GL20.GL_EQUAL);
+		Gdx.gl.glDepthFunc(GL20.GL_EQUAL);
 	}
 	
 	/**
 	 * call this AFTER batch.flush/end
 	 */
 	public static void resetMask(){
-		Gdx.gl20.glDisable(GL20.GL_DEPTH_TEST);
+		Gdx.gl.glDisable(GL20.GL_DEPTH_TEST);
 	}
 }
