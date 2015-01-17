70b717d4c30b33d0d1fd424ca9780448108bd69e
chrislo27 <kinchrislo@gmail.com>
2015-01-17 15:11:15 -0800

    Simplify sprite rendering

--- src/stray/util/render/BlendMaskUtil.java
+++ src/stray/util/render/BlendMaskUtil.java
@@ -26,28 +26,11 @@ public class BlendMaskUtil {
 		drawAlphaMask(batch, alphamask, x, y, alphamask.getWidth(), alphamask.getHeight());
 	}
 
-	public static void drawSpriteToMask(Batch batch, Texture sprite, int x, int y, int width, int height) {
+	public static void beginSprites(Batch batch) {
 		// now that the buffer has our alpha, we simply draw the sprite with the
 		// mask applied
 		Gdx.gl.glColorMask(true, true, true, true);
 		batch.setBlendFunction(GL20.GL_DST_ALPHA, GL20.GL_ONE_MINUS_DST_ALPHA);
-
-		// The scissor test is optional, but it depends
-		Gdx.gl.glEnable(GL20.GL_SCISSOR_TEST);
-		Gdx.gl.glScissor(x, y, width, height);
-
-		// draw our sprite to be masked
-		batch.draw(sprite, 0, 0, 250, 250);
-
-		// remember to flush before changing GL states again
-		batch.flush();
-
-		// disable scissor before continuing
-		Gdx.gl.glDisable(GL20.GL_SCISSOR_TEST);
-	}
-	
-	public static void drawSpriteToMask(Batch batch, Texture sprite, int x, int y){
-		drawSpriteToMask(batch, sprite, x, y, sprite.getWidth(), sprite.getHeight());
 	}
 
 }
