e89acaec866314927d9648dbc75cf70ca9943637
chrislo27 <kinchrislo@gmail.com>
2015-01-23 19:13:20 -0800

    Super ultra nerfed FLUDD cooldown

--- src/stray/augment/FLUDDAugment.java
+++ src/stray/augment/FLUDDAugment.java
@@ -39,7 +39,7 @@ public class FLUDDAugment extends Augment {
 		
 		if(player.health < player.maxhealth){
 			// 25% regen boost
-			player.health += (Gdx.graphics.getRawDeltaTime() / (EntityPlayer.SECONDS_TO_REGEN * 0.25f));
+			player.health += (Gdx.graphics.getRawDeltaTime());
 			player.health = MathUtils.clamp(player.health, 0f, player.maxhealth);
 		}
 	}
