b2215455de92991e24ed1f73aaa91f9b51d2ddba
chrislo27 <kinchrislo@gmail.com>
2015-01-26 19:53:59 -0800

    CaptureStream now CORRECTLY outputs the log instead of its toString

--- src/stray/util/CaptureStream.java
+++ src/stray/util/CaptureStream.java
@@ -10,23 +10,32 @@ public class CaptureStream extends OutputStream {
 	private Consumer consumer;
 	private PrintStream old;
 
+	private StringBuilder total;
+
 	public CaptureStream(Consumer consumer, PrintStream old) {
 		buffer = new StringBuilder(256);
+		total = new StringBuilder(Short.MAX_VALUE);
 		this.old = old;
 		this.consumer = consumer;
 	}
 
-	 @Override
-     public void write(int b) throws IOException {
-         char c = (char) b;
-         String value = Character.toString(c);
-         buffer.append(value);
-         if (value.equals("\n")) {
-             consumer.appendText(buffer.toString());
-             buffer.delete(0, buffer.length());
-         }
-         old.print(c);
-     }  
+	@Override
+	public void write(int b) throws IOException {
+		char c = (char) b;
+		String value = Character.toString(c);
+		buffer.append(value);
+		total.append(value);
+		if (value.equals("\n")) {
+			consumer.appendText(buffer.toString());
+			buffer.delete(0, buffer.length());
+		}
+		old.print(c);
+	}
+
+	@Override
+	public String toString() {
+		return total.toString();
+	}
 
 	public static interface Consumer {
 
