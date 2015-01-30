902f2d4b745e2637c90a1bc83ebe1a4d0b0d6f57
chrislo27 <kinchrislo@gmail.com>
2015-01-29 19:35:05 -0800

    Added catch clause when parsing versions

--- src/stray/util/version/VersionGetter.java
+++ src/stray/util/version/VersionGetter.java
@@ -12,9 +12,8 @@ import stray.Settings;
 import com.badlogic.gdx.utils.JsonReader;
 import com.badlogic.gdx.utils.JsonValue;
 
-
 public class VersionGetter {
-	
+
 	private static VersionGetter instance;
 
 	private VersionGetter() {
@@ -29,24 +28,24 @@ public class VersionGetter {
 	}
 
 	private VersionDiff difference = VersionDiff.CHECKING;
-	
+
 	private void loadResources() {
-		
+
 	}
-	
-	public static VersionDiff getDiff(){
+
+	public static VersionDiff getDiff() {
 		return instance().difference;
 	}
 
 	/**
 	 * NOTE: This method blocks until it fails or completes
 	 */
-	public void getVersionFromServer(){
+	public void getVersionFromServer() {
 		final String path = "https://raw.githubusercontent.com/chrislo27/Stray-core/master/version.txt";
 		long start = System.currentTimeMillis();
 		try {
-			BufferedReader br = new BufferedReader(new InputStreamReader(new URL(path)
-					.openStream()));
+			BufferedReader br = new BufferedReader(
+					new InputStreamReader(new URL(path).openStream()));
 
 			StringBuilder file = new StringBuilder();
 			String inputline;
@@ -54,18 +53,17 @@ public class VersionGetter {
 				file.append(inputline);
 
 			br.close();
-			
+
 			Main.logger.info("Finished getting version, took "
 					+ (System.currentTimeMillis() - start) + " ms");
-			
+
 			JsonValue value = new JsonReader().parse(file.toString());
 
 			Main.latestVersion = value.getString("version", "");
 
 			difference = compareVersions(Main.version, Main.latestVersion);
-			
-			if (Settings.debug) Main.logger
-					.debug("JSON obtained from host: " + file.toString());
+
+			if (Settings.debug) Main.logger.debug("JSON obtained from host: " + file.toString());
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 			difference = VersionDiff.INVALID;
@@ -77,41 +75,43 @@ public class VersionGetter {
 			difference = VersionDiff.INVALID;
 		}
 	}
-	
+
 	public static VersionDiff compareVersions(String current, String server) {
 		int i = compareVersionsInt(current, server);
-		if(i == 0){
+		if (i == 0) {
 			return VersionDiff.EQUAL;
-		}else if(i > 0){
+		} else if (i == 1) {
 			return VersionDiff.NEWER;
-		}else if(i < 0){
+		} else if (i == -1) {
 			return VersionDiff.OLDER;
-		}else return VersionDiff.INVALID;
+		} else return VersionDiff.INVALID;
 	}
-	
+
 	/**
-	 * uses integers to determine version differences
-	 * 0 is same, positive is the build version is newer than server, negative is the build version is older than server
+	 * uses integers to determine version differences 0 is same, positive is the
+	 * build version is newer than server, negative is the build version is
+	 * older than server
+	 * 
 	 * @param current
 	 * @param server
 	 * @return
 	 */
-	public static int compareVersionsInt(String current, String server){
+	public static int compareVersionsInt(String current, String server) {
 		String str1 = current;
 		String str2 = server;
-		
+
 		// bleh way of handling alpha and beta
-		if(str1.endsWith("-alpha")){
+		if (str1.endsWith("-alpha")) {
 			str1 = str1.replace("-alpha", ".1");
-		}else if(str1.endsWith("-beta")){
+		} else if (str1.endsWith("-beta")) {
 			str1 = str1.replace("-beta", ".2");
 		}
-		if(str2.endsWith("-alpha")){
+		if (str2.endsWith("-alpha")) {
 			str2 = str2.replace("-alpha", ".1");
-		}else if(str2.endsWith("-beta")){
+		} else if (str2.endsWith("-beta")) {
 			str2 = str2.replace("-beta", ".2");
 		}
-		
+
 		String[] vals1 = str1.split("\\.");
 		String[] vals2 = str2.split("\\.");
 		int i = 0;
@@ -120,15 +120,22 @@ public class VersionGetter {
 		while (i < vals1.length && i < vals2.length && vals1[i].equals(vals2[i])) {
 			i++;
 		}
-		// compare first non-equal ordinal number
-		if (i < vals1.length && i < vals2.length) {
-			int diff = Integer.valueOf(vals1[i]).compareTo(Integer.valueOf(vals2[i]));
-			return Integer.signum(diff);
-		}
-		// the strings are equal or one string is a substring of the other
-		// e.g. "1.2.3" = "1.2.3" or "1.2.3" < "1.2.3.4"
-		else {
-			return Integer.signum(vals1.length - vals2.length);
+
+		try {
+			// compare first non-equal ordinal number
+			if (i < vals1.length && i < vals2.length) {
+				int diff = Integer.valueOf(vals1[i]).compareTo(Integer.valueOf(vals2[i]));
+				return Integer.signum(diff);
+			}
+			// the strings are equal or one string is a substring of the other
+			// e.g. "1.2.3" = "1.2.3" or "1.2.3" < "1.2.3.4"
+			else {
+				return Integer.signum(vals1.length - vals2.length);
+			}
+		} catch (NumberFormatException e) {
+			Main.logger.error(
+					"Failed to parse versions \"" + current + "\" and \"" + server + "\"", e);
+			return 404;
 		}
 	}
 }
