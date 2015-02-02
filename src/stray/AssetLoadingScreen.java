0fa01aabd1e9f31a6d414444431af9d99de6e6c9
chrislo27 <kinchrislo@gmail.com>
2015-02-02 09:17:26 -0800

    Test

--- src/stray/AssetLoadingScreen.java
+++ src/stray/AssetLoadingScreen.java
@@ -6,10 +6,16 @@ import java.util.Map;
 import stray.blocks.Block;
 import stray.blocks.Blocks;
 import stray.util.AssetLogger;
+import stray.util.ScreenshotFactory;
+import stray.util.Utils;
 import stray.world.World;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.PixmapIO;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.utils.Logger;
 
 public class AssetLoadingScreen extends MiscLoadingScreen {
@@ -77,6 +83,23 @@ public class AssetLoadingScreen extends MiscLoadingScreen {
 		main.drawTextBg(percent, Gdx.graphics.getWidth() / 2
 				- (main.font.getBounds(percent).width / 2), Gdx.graphics.getHeight() / 2 - 60);
 
+//		if(Gdx.input.isKeyJustPressed(Keys.S)){
+//			Texture tex = new Texture("images/blocks/portal/portal.png");
+//			for(int i = 0; i < 32; i++){
+//				Main.logger.debug("begin spiral " + (i + 1));
+//				main.batch.setColor(1, 1, 1, 1);
+//				Utils.drawRotated(main.batch, tex, 0 - (World.tilesizex / 2f),
+//						(0) - (World.tilesizey / 2f),
+//						tex.getWidth(), tex.getHeight(), i * 11.25f, true);
+//				main.batch.flush();
+//				Main.logger.debug("begin spiral screenshot" + (i + 1));
+//				Pixmap pixmap = ScreenshotFactory.getScreenshot(0, 0, 64, 64, true);
+//	            PixmapIO.writePNG(new FileHandle("spiral/number" + (i + 1) + ".png"), pixmap);
+//	            pixmap.dispose();
+//				Main.logger.debug("end spiral " + (i + 1));
+//			}
+//		}
+		
 		main.batch.end();
 	}
 
