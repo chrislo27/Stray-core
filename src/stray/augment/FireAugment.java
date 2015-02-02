e05fd0f52178de3b95ee1efffbcf46ff3fe619ea
chrislo27 <kinchrislo@gmail.com>
2015-02-02 11:45:42 -0800

    Flamethrower augment now hurts player (6.4 seconds from cool to death)

--- src/stray/augment/FireAugment.java
+++ src/stray/augment/FireAugment.java
@@ -1,11 +1,15 @@
 package stray.augment;
 
-import com.badlogic.gdx.graphics.Color;
-
+import stray.Main;
+import stray.entity.EntityPlayer;
 import stray.world.World;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 
-public class FireAugment extends Augment{
+public class FireAugment extends Augment {
+
+	private static final float DPS = (1f / 6.4f);
 
 	@Override
 	public void onActivateStart(World world) {
@@ -13,6 +17,10 @@ public class FireAugment extends Augment{
 
 	@Override
 	public void onActivate(World world) {
+		EntityPlayer player = world.getPlayer();
+		
+		player.heal(-(DPS * Gdx.graphics.getDeltaTime())
+				+ -(Gdx.graphics.getDeltaTime() / EntityPlayer.SECONDS_TO_REGEN));
 	}
 
 	@Override
@@ -21,13 +29,14 @@ public class FireAugment extends Augment{
 
 	@Override
 	public void onActivateTick(World world) {
+
 	}
 
 	@Override
 	public String getName() {
 		return "augment.name.fire";
 	}
-	
+
 	@Override
 	public Color getColor() {
 		return Augment.reused.set(1, 102 / 255f, 0, 1);
