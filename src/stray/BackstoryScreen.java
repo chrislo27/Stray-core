582e534f97827b93abc1674e964a14513b2d67ab
chrislo27 <kinchrislo@gmail.com>
2015-02-02 17:23:55 -0800

    Removed unnecessary pause when loading levels

--- src/stray/BackstoryScreen.java
+++ src/stray/BackstoryScreen.java
@@ -16,11 +16,9 @@ public class BackstoryScreen extends MiscLoadingScreen {
 	}
 
 	private String story = "";
-	private float time = 0;
 	private Updateable next;
 	private FileHandle level;
 	private boolean loaded = false;
-	private final float TIMETAKEN = 1.5f;
 
 	public void render(float delta) {
 		super.render(delta);
@@ -31,30 +29,23 @@ public class BackstoryScreen extends MiscLoadingScreen {
 				Translator.getMsg("backstory." + story), Gdx.graphics.getWidth() / 4f,
 				Main.convertY(Gdx.graphics.getHeight() * 0.4f), Gdx.graphics.getWidth() / 2f);
 
-		if (time <= 0.25f) {
-			main.font.setColor(1, 1, 1, 1f - (MathUtils.clamp(time, 0, 0.25f) * 4));
-			main.drawCentered(Translator.getMsg("conversation.next"), Gdx.graphics.getWidth() / 2,
-					Main.convertY(Gdx.graphics.getHeight() * 0.8f));
-			main.font.setColor(1, 1, 1, 1);
-			main.batch.setColor(Color.BLACK);
-			main.fillRect(Gdx.graphics.getWidth() - 192, 0, 192, 128);
-			main.batch.setColor(Color.WHITE);
+		main.font.setColor(1, 1, 1, 1);
+		main.drawCentered(Translator.getMsg("conversation.next"), Gdx.graphics.getWidth() / 2,
+				Main.convertY(Gdx.graphics.getHeight() * 0.8f));
+		main.font.setColor(1, 1, 1, 1);
+		main.batch.setColor(Color.BLACK);
+		main.fillRect(Gdx.graphics.getWidth() - 192, 0, 192, 128);
+		main.batch.setColor(Color.WHITE);
 
-		}
 		main.batch.end();
 
-		if (time > 0) {
-			time -= Gdx.graphics.getDeltaTime();
-			if (time < (TIMETAKEN / 2f) && !loaded) {
-				Main.GAME.world.load(level);
-				loaded = true;
-			}
-			if (time < 0) time = 0;
+		if (!loaded) {
+			Main.GAME.world.load(level);
+			loaded = true;
 		}
 	}
 
 	public void prepare(String st, FileHandle level) {
-		time = TIMETAKEN;
 		story = st;
 		next = Main.GAME;
 		this.level = level;
@@ -63,13 +54,12 @@ public class BackstoryScreen extends MiscLoadingScreen {
 
 	public void renderUpdate() {
 		super.renderUpdate();
-		if (time <= 0) {
+		if (loaded) {
 			if (Gdx.input.isKeyJustPressed(Keys.SPACE) || Gdx.input.isKeyJustPressed(Keys.ENTER)) {
 				main.transition(new FadeIn(), new FadeOut(), next);
 			}
 		}
 
-		
 	}
 
 }
