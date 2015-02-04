468d40ecb608db2df9f7fbc9e8420b0c3d1eefb0
chrislo27 <kinchrislo@gmail.com>
2015-02-04 09:23:44 -0800

    BlockFadeable has new methods for getting alpha

--- src/stray/blocks/BlockFadeable.java
+++ src/stray/blocks/BlockFadeable.java
@@ -10,17 +10,19 @@ public class BlockFadeable extends Block {
 
 	@Override
 	public void render(World world, int x, int y) {
-		if (!isSolid(world, x, y)) {
-			world.main.batch.setColor(1, 1, 1, 0.075f);
-		} else {
-			if (Block.entityIntersects(world, x + World.tilepartx, y + World.tileparty,
-					world.getPlayer(), 1f - (World.tilepartx * 2), 1f - (World.tileparty * 2))) {
-				world.main.batch.setColor(1, 1, 1, 0.5f);
-			}
-		}
+		world.batch.setColor(1, 1, 1, getAlpha(world, x, y));
+		
 		super.render(world, x, y);
 
-		world.main.batch.setColor(1, 1, 1, 1);
+		world.batch.setColor(1, 1, 1, 1);
+	}
+	
+	protected float getAlpha(World world, int x, int y){
+		if(!isSolid(world, x, y)) return 0.075f;
+		if(Block.entityIntersects(world, x + World.tilepartx, y + World.tileparty,
+					world.getPlayer(), 1f - (World.tilepartx * 2), 1f - (World.tileparty * 2))) return 0.5f;
+		
+		return 1;
 	}
 
 }
