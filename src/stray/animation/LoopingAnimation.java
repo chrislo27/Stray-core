cd0571d2789339daa2a88edbf7f8094384d4cdeb
chrislo27 <kinchrislo@gmail.com>
2015-02-08 14:31:51 -0800

    Now extends OneTimeAnimation but starts immediately

--- src/stray/animation/LoopingAnimation.java
+++ src/stray/animation/LoopingAnimation.java
@@ -4,20 +4,11 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.Disposable;
 
-public class LoopingAnimation extends Animation{
+public class LoopingAnimation extends OneTimeAnimation{
 
 	public LoopingAnimation(float delay, int count, String path, boolean usesRegion) {
 		super(delay, count, path, usesRegion);
-	}
-
-	@Override
-	public TextureRegion getCurrentFrame() {
-		return getCurrentFrame(framedelay);
-	}
-
-	public TextureRegion getCurrentFrame(float delay) {
-		long i = (long) (System.currentTimeMillis() / (delay * 1000d));
-		return frames[(int) ((frames.length - 1) - (i % frames.length))];
+		start();
 	}
 
 }
