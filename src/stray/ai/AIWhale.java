91d476fd71c07cc1cec3359883cb69ce1acf6456
chrislo27 <kinchrislo@gmail.com>
2015-02-08 19:26:53 -0800

    Whale AI now breathes fire left properly

--- src/stray/ai/AIWhale.java
+++ src/stray/ai/AIWhale.java
@@ -1,7 +1,6 @@
 package stray.ai;
 
-import com.badlogic.gdx.math.MathUtils;
-
+import stray.Main;
 import stray.entity.Entity;
 import stray.entity.EntityFlame;
 import stray.entity.EntityLiving;
@@ -22,25 +21,26 @@ public class AIWhale extends AIDumbEnemy {
 	public void tickUpdate() {
 		super.tickUpdate();
 
-		if (Math.abs(e.world.getPlayer().x - (e.x + e.sizex)) < 7.5f) {
-			if (((EntityLiving) e).facing == Direction.RIGHT) {
-				if (e.world.getPlayer().x >= e.x + e.sizex) {
-					EntityFlame flame = new EntityFlame(e.world, e.x + 8.703125f, e.y + 3.9375f);
-					flame.velox = (e.world.getPlayer().x - flame.x) * 2f;
-					flame.veloy = ((e.world.getPlayer().y + e.world.getPlayer().sizex) - flame.y) * 1.25f;
+		if (((EntityLiving) e).facing == Direction.RIGHT) {
+			if (e.world.getPlayer().x >= e.x + e.sizex
+					&& Math.abs(e.world.getPlayer().x - (e.x + e.sizex)) < 10f) {
+				EntityFlame flame = new EntityFlame(e.world, e.x + 8.703125f, e.y + 3.9375f);
+				flame.velox = (e.world.getPlayer().x - flame.x) * 2f;
+				flame.veloy = ((e.world.getPlayer().y + e.world.getPlayer().sizex) - flame.y) * 1.25f;
 
-					e.world.entities.add(flame);
-				}
-			} else if (((EntityLiving) e).facing == Direction.LEFT) {
-				if (e.world.getPlayer().x + e.world.getPlayer().sizex <= e.x) {
-					EntityFlame flame = new EntityFlame(e.world, e.x + 0.875f, e.y + 3.9375f);
-					flame.velox = (e.world.getPlayer().x - flame.x) * 2f;
-					flame.veloy = ((e.world.getPlayer().y + e.world.getPlayer().sizex) - flame.y) * 1.25f;
+				e.world.entities.add(flame);
+			}
+		} else if (((EntityLiving) e).facing == Direction.LEFT) {
+			if (e.world.getPlayer().x + e.world.getPlayer().sizex <= e.x
+					&& Math.abs((e.world.getPlayer().x + e.world.getPlayer().sizex) - e.x) < 10f) {
+				EntityFlame flame = new EntityFlame(e.world, e.x + 0.875f, e.y + 3.9375f);
+				flame.velox = (e.world.getPlayer().x - flame.x) * 2f;
+				flame.veloy = ((e.world.getPlayer().y + e.world.getPlayer().sizex) - flame.y) * 1.25f;
 
-					e.world.entities.add(flame);
-				}
+				e.world.entities.add(flame);
 			}
 		}
+
 	}
 
 }
