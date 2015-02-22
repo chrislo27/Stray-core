d8447c41fc9c7ba83e8996bda2d1059812496d66
chrislo27 <kinchrislo@gmail.com>
2015-02-21 17:11:16 -0800

    Air vents now are blocked by solid blocks

--- src/stray/blocks/BlockAirVent.java
+++ src/stray/blocks/BlockAirVent.java
@@ -11,20 +11,34 @@ public class BlockAirVent extends Block {
 	public BlockAirVent(String path) {
 		super(path);
 	}
+	
+	public static final int range = 3;
 
 	@Override
 	public void tickUpdate(World world, int x, int y) {
-		for (Entity e : world.entities) {
-			if (Block.entityIntersects(world, x - World.tilepartx, y, e, 1 + (world.tilepartx * 2), -3f)) {
-				e.veloy -= 1f;
+		float newRange = range;
+		for(int i = 0; i < range; i++){
+			if(world.getBlock(x, y - i - 1).isSolid(world, x, y - i - 1)){
+				newRange = i;
+				break;
 			}
 		}
 		
-		if(world.tickTime % 2 == 0) return;
+		if(newRange <= 0) return;
 		
-		world.particles.add(ParticlePool.obtain().setTexture("airwhoosh").setPosition(x + 0.5f + MathUtils.random(-0.25f, 0.25f), y)
+		for (Entity e : world.entities) {
+			if (Block.entityIntersects(world, x - World.tilepartx, y - newRange, e, 1 + (world.tilepartx * 2),
+					newRange)) {
+				e.veloy -= 1f;
+			}
+		}
+
+		if (world.tickTime % 2 == 0) return;
+
+		world.particles.add(ParticlePool.obtain().setTexture("airwhoosh")
+				.setPosition(x + 0.5f + MathUtils.random(-0.25f, 0.25f), y)
 				.setVelocity(MathUtils.random(-0.075f, 0.075f), -10f).setLifetime(0.33333333f)
-				.setStartScale(2f).setEndScale(2f).setAlpha(0.5f));
+				.setStartScale(2f).setEndScale(2f).setAlpha(0.5f).setDestroyOnBlock(true));
 	}
 
 }
