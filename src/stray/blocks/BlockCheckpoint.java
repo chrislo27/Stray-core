3f44aa6e143403e300550ec8c1c9bfd1e0edff8a
chrislo27 <kinchrislo@gmail.com>
2015-02-21 18:37:29 -0800

    Shrunk particles

--- src/stray/blocks/BlockCheckpoint.java
+++ src/stray/blocks/BlockCheckpoint.java
@@ -38,7 +38,7 @@ public class BlockCheckpoint extends Block {
 					.setLifetime(1.75f + MathUtils.random(1.25f))
 					.setPosition(x + MathUtils.random(0.2f, 0.8f), y + MathUtils.random(0.2f, 0.8f))
 					.setVelocity(1.25f * MathUtils.randomSign() * MathUtils.random(0.05f, 1f),
-							-1f * MathUtils.random(0.8f, 1.2f)).setTint(1, 1, 1, 1).setAlpha(0.85f));
+							-1f * MathUtils.random(0.8f, 1.2f)).setTint(1, 1, 1, 1).setAlpha(0.85f).setStartScale(0.75f).setEndScale(0.5f));
 		}
 	}
 
