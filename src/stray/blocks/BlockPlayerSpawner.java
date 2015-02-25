32800a7bb3a6a734becec66bcbf0d198afd22c4b
chrislo27 <kinchrislo@gmail.com>
2015-02-24 21:09:17 -0800

    Formatting

--- src/stray/blocks/BlockPlayerSpawner.java
+++ src/stray/blocks/BlockPlayerSpawner.java
@@ -3,8 +3,7 @@ package stray.blocks;
 import stray.entity.Entity;
 import stray.world.World;
 
-
-public class BlockPlayerSpawner extends BlockSpawner{
+public class BlockPlayerSpawner extends BlockSpawner {
 
 	public BlockPlayerSpawner(String path) {
 		super(path);
@@ -14,16 +13,16 @@ public class BlockPlayerSpawner extends BlockSpawner{
 	public Entity getEntity(World world, int x, int y) {
 		return null;
 	}
-	
+
 	@Override
-	public void tickUpdate(World world, int x, int y){
-		if(world.getPlayer() == null){
+	public void tickUpdate(World world, int x, int y) {
+		if (world.getPlayer() == null) {
 			world.addPlayer();
 		}
 		world.getPlayer().x = x;
 		world.getPlayer().y = y;
 		world.setCheckpoint();
-		
+
 		world.setBlock(null, x, y);
 	}
 
