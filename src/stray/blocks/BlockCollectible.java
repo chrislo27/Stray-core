fd66591504fbd5b5b252f4a337aa3a23323eae8f
chrislo27 <kinchrislo@gmail.com>
2015-02-26 17:21:22 -0800

    Collectible now deletes itself when collected

--- src/stray/blocks/BlockCollectible.java
+++ src/stray/blocks/BlockCollectible.java
@@ -16,10 +16,9 @@ public class BlockCollectible extends Block{
 	
 	@Override
 	public void tickUpdate(World world, int x, int y) {
-		if(world.getMeta(x, y) != null) return;
 		if(Block.entityIntersects(world, x, y, world.getPlayer())){
 			world.global.setInt(collectible, world.global.getInt(collectible) + 1);
-			world.setMeta("" + this.hashCode(), x, y); // this can be !null
+			world.setBlock(null, x, y);
 		}
 	}
 
@@ -30,7 +29,6 @@ public class BlockCollectible extends Block{
 	
 	@Override
 	public void render(World world, int x, int y){
-		if(world.getMeta(x, y) != null) return;
 		super.render(world, x, y);
 	}
 
