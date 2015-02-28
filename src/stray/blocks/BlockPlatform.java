1c69ce59c8070bed3c1545391f1aa9e3ba1e39fc
chrislo27 <kinchrislo@gmail.com>
2015-02-28 14:02:56 -0800

    Added a directional block

--- src/stray/blocks/BlockPlatform.java
+++ src/stray/blocks/BlockPlatform.java
@@ -8,7 +8,7 @@ import stray.util.MathHelper;
 import stray.util.Utils;
 import stray.world.World;
 
-public class BlockPlatform extends Block {
+public class BlockPlatform extends BlockDirectional {
 
 	public BlockPlatform(String path) {
 		super(path);
@@ -19,51 +19,6 @@ public class BlockPlatform extends Block {
 		return MathUtils.clamp(world.getMeta(x, y), SolidFaces.NONE, SolidFaces.ALL);
 	}
 
-	@Override
-	public void renderWithOffset(World world, int x, int y, float offx, float offy) {
-		if (isSolid(world, x, y) <= 0) {
-			Utils.drawRotated(
-					world.batch,
-					world.main.manager.get(sprites.get("defaulttex"), Texture.class),
-					x * world.tilesizex - world.camera.camerax + offx,
-					Main.convertY((y * world.tilesizey - world.camera.cameray) + World.tilesizey
-							+ offy), World.tilesizex, World.tilesizey, World.tilesizex / 2f,
-					World.tilesizey / 2f, MathHelper.getNumberFromTime(2) * 360, true);
-			return;
-		}
-
-		if ((isSolid(world, x, y) & SolidFaces.UP) == SolidFaces.UP) {
-			Utils.drawRotated(
-					world.batch,
-					world.main.manager.get(sprites.get("defaulttex"), Texture.class),
-					x * world.tilesizex - world.camera.camerax + offx,
-					Main.convertY((y * world.tilesizey - world.camera.cameray) + World.tilesizey
-							+ offy), World.tilesizex, World.tilesizey, 0, true);
-		}
-		if ((isSolid(world, x, y) & SolidFaces.DOWN) == SolidFaces.DOWN) {
-			Utils.drawRotated(
-					world.batch,
-					world.main.manager.get(sprites.get("defaulttex"), Texture.class),
-					x * world.tilesizex - world.camera.camerax + offx,
-					Main.convertY((y * world.tilesizey - world.camera.cameray) + World.tilesizey
-							+ offy), World.tilesizex, World.tilesizey, 180, true);
-		}
-		if ((isSolid(world, x, y) & SolidFaces.LEFT) == SolidFaces.LEFT) {
-			Utils.drawRotated(
-					world.batch,
-					world.main.manager.get(sprites.get("defaulttex"), Texture.class),
-					x * world.tilesizex - world.camera.camerax + offx,
-					Main.convertY((y * world.tilesizey - world.camera.cameray) + World.tilesizey
-							+ offy), World.tilesizex, World.tilesizey, 270, true);
-		}
-		if ((isSolid(world, x, y) & SolidFaces.RIGHT) == SolidFaces.RIGHT) {
-			Utils.drawRotated(
-					world.batch,
-					world.main.manager.get(sprites.get("defaulttex"), Texture.class),
-					x * world.tilesizex - world.camera.camerax + offx,
-					Main.convertY((y * world.tilesizey - world.camera.cameray) + World.tilesizey
-							+ offy), World.tilesizex, World.tilesizey, 90, true);
-		}
-	}
+	
 
 }
