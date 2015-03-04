ff9834ca94b9bd068e08013ce850584f3d2dc3b6
chrislo27 <kinchrislo@gmail.com>
2015-03-03 19:00:45 -0800

    harumph

---
+++ src/stray/util/OpenTheEasterEggYoutubeChannelThatIsNotEvenMine.java
@@ -0,0 +1,18 @@
+package stray.util;
+
+import com.badlogic.gdx.Gdx;
+
+
+public final class OpenTheEasterEggYoutubeChannelThatIsNotEvenMine {
+	
+	private OpenTheEasterEggYoutubeChannelThatIsNotEvenMine(){}
+	
+	public static void openMyChannel(){
+		Gdx.net.openURI("https://www.youtube.com/channel/UCH-IGuVo6vXmsKhynFUkaxQ");
+	}
+	
+	public static void openBeysChannel(){
+		Gdx.net.openURI("https://www.youtube.com/channel/UC-wSaeK72L0H_H-QEv60mdg");
+	}
+	
+}
